6b3ec2ec3a5dc657e59cf6934063ef8849227c42
Xiang Chen <chenxiang1984@gmail.com>
2020-07-20 15:49:58 +0100

    Professionlism

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Clean Coder Review Professionalism",
+        "excerpt":"I recently read this book “Clean Coder” from Robert C. Martin, who is also the author of “Clean Code” which someone may refer it as the bible of the programmers. To me, the experience of reading this book is quite special, I felt being taken under the wings and mentored...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Clean-Coder-Review-Professionalism/",
+        "teaser": null
+      }]

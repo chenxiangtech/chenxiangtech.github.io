8c081b66b2ee637295bcc76c4a44c1bb82d92922
Xiang Chen <chenxiang1984@gmail.com>
2020-07-22 17:14:27 +0100

    Design pattern

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "https://chenxiangtech.github.io/Professionalism-Starts-With-Taking-Responsibility/",
         "teaser": null
+      },{
+        "title": "The 23 Gang Of Four Design Patterns",
+        "excerpt":"Work In Progress The 23 Gang of Four Design Patterns have been refered as must known for every technical professional. I’m going to document my understanding on these patterns, as I notice that for some of them there might be different interpretations which all make good sense. So the goal...","categories": [],
+        "tags": [],
+        "url": "https://chenxiangtech.github.io/The-23-Gang-of-Four-Design-Patterns/",
+        "teaser": null
       }]

a9fd55d8e535f7025463703aeff0ca2e7921dcc5
Xiang Chen <chenxiang1984@gmail.com>
2020-07-22 12:07:41 +0100

    Prof rev 2

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Clean Coder Review Professionalism",
+        "title": "Professionalism Starts With Taking Responsibility",
         "excerpt":"I recently read this book “Clean Coder” from Robert C. Martin, who is also the author of “Clean Code” which someone may refer it as the bible of the programmers. To me, the experience of reading this book is quite special, I felt being taken under the wings and mentored...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Clean-Coder-Review-Professionalism/",
+        "url": "http://localhost:4000/Professionalism-Starts-With-Taking-Responsibility/",
         "teaser": null
       }]

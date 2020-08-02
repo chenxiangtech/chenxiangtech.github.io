cc875f8730c104d4126091c421bdaa173f5d4efa
Xiang Chen <chenxiang1984@gmail.com>
2020-08-02 15:15:02 +0100

    Date of Singleton

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,12 +5,6 @@ var store = [{
         "url": "https://chenxiangtech.github.io/Professionalism-Starts-With-Responsibility/",
         "teaser": null
       },{
-        "title": "Design Pattern 1 Singleton",
-        "excerpt":"Singleton probably is the most referred, and seen design pattern. Summarized in one sentence, there will one and only one initialized instance in the runtime environment. Pros There will be only one initialized object in the memory, obviously this will save memory space. On top of that, in the scenario...","categories": [],
-        "tags": [],
-        "url": "https://chenxiangtech.github.io/Design-Pattern-1-Singleton/",
-        "teaser": null
-      },{
         "title": "No Boundaries",
         "excerpt":"I recently was updating my Linkedin profile, and therefore received several phone calls from some recruiters. It’s good to be in conversations where people are showing interest in you, and it all went like breeze. There was one particular question that I didn’t think too much at the time, but...","categories": [],
         "tags": [],
@@ -21,5 +15,11 @@ var store = [{
         "excerpt":"Work In Progress The 23 Gang of Four Design Patterns have been refered as must known for every technical professional. I’m going to document my understanding on these patterns, as I notice that for some of them there might be different interpretations which all make good sense. So the goal...","categories": [],
         "tags": [],
         "url": "https://chenxiangtech.github.io/The-23-Gang-of-Four-Design-Patterns/",
+        "teaser": null
+      },{
+        "title": "Design Pattern 1 Singleton",
+        "excerpt":"Singleton probably is the most referred, and seen design pattern. Summarized in one sentence, there will one and only one initialized instance in the runtime environment. Pros There will be only one initialized object in the memory, obviously this will save memory space. On top of that, in the scenario...","categories": [],
+        "tags": [],
+        "url": "https://chenxiangtech.github.io/Design-Pattern-1-Singleton/",
         "teaser": null
       }]

0732aa7c64c89a881385cda61f24a4b010c14ce6
Xiang Chen <chenxiang1984@gmail.com>
2020-08-02 15:12:17 +0100

    production

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Professionalism Starts With Responsibility",
         "excerpt":"I recently read the book “Clean Coder” from Robert C. Martin, who is also the author of “Clean Code” which someone may refer it as the bible of the programmers. To me, the experience of reading this book is quite special, I felt being taken under the wings and mentored...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Professionalism-Starts-With-Responsibility/",
+        "url": "https://chenxiangtech.github.io/Professionalism-Starts-With-Responsibility/",
         "teaser": null
       },{
         "title": "Design Pattern 1 Singleton",
         "excerpt":"Singleton probably is the most referred, and seen design pattern. Summarized in one sentence, there will one and only one initialized instance in the runtime environment. Pros There will be only one initialized object in the memory, obviously this will save memory space. On top of that, in the scenario...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Design-Pattern-1-Singleton/",
+        "url": "https://chenxiangtech.github.io/Design-Pattern-1-Singleton/",
         "teaser": null
       },{
         "title": "No Boundaries",
         "excerpt":"I recently was updating my Linkedin profile, and therefore received several phone calls from some recruiters. It’s good to be in conversations where people are showing interest in you, and it all went like breeze. There was one particular question that I didn’t think too much at the time, but...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/No-boundaries/",
+        "url": "https://chenxiangtech.github.io/No-boundaries/",
         "teaser": null
       },{
         "title": "The 23 Gang Of Four Design Patterns",
         "excerpt":"Work In Progress The 23 Gang of Four Design Patterns have been refered as must known for every technical professional. I’m going to document my understanding on these patterns, as I notice that for some of them there might be different interpretations which all make good sense. So the goal...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/The-23-Gang-of-Four-Design-Patterns/",
+        "url": "https://chenxiangtech.github.io/The-23-Gang-of-Four-Design-Patterns/",
         "teaser": null
       }]
